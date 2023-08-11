610c826c83bae274c992bdac4b0554cab9118255
Ryan <ryan.rosa.profissional@gmail.com>
2023-08-11 06:36:04 -0300

    projeto inicializado

---
+++ src/main/java/com/zicartola/BaseTudo/BaseTudoApplication.java
@@ -0,0 +1,13 @@
+package com.zicartola.BaseTudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseTudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseTudoApplication.class, args);
+	}
+
+}
